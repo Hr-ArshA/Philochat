488f2c48472e15cd8114ae52f1c4c566895cff89
Hr-ArshA <a_sh1379@yahoo.com>
2023-09-04 02:57:17 +0330

    makeing chat models

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,93 @@
 from django.contrib import admin
+from .models import GroupChat, GroupChatMessage, PrivateChat, PrivateChatMessage
+from datetime import datetime
+from django.utils.html import format_html
 
 # Register your models here.
+
+class GroupChatAdmin(admin.ModelAdmin):
+    model = GroupChat
+    list_display = ("name", "members_count", 'chat_id', "time")
+    list_filter = ("members", "timestamp")
+    search_fields = ("name",)
+
+    def members_count(self, model: GroupChat):
+        return model.members.count()
+
+    def time(self, model: GroupChat):
+        date: datetime = model.timestamp.astimezone()
+        format_date = datetime.strftime(date, "%b %d, %H:%M:%S")
+        return format_date
+
+    fieldsets = ((None, {"fields": ("name", 'members',)}),)
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("name", 'members')}
+        ),
+    )
+
+
+class PrivateChatAdmin(admin.ModelAdmin):
+    model = PrivateChat
+    list_display = ("author", 'contact', 'chat_id',"time")
+    list_filter = ("author", 'contact', "timestamp")
+
+    def time(self, model: PrivateChat):
+        date: datetime = model.timestamp.astimezone()
+        format_date = datetime.strftime(date, "%b %d, %H:%M:%S")
+        return format_date
+
+    fieldsets = ((None, {"fields": ("author", 'contact', 'chat_id')}),)
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("author", 'contact', )}
+        ),
+    )
+
+
+class PrivateChatMessageAdmin(admin.ModelAdmin):
+    model = PrivateChatMessage
+    list_display = ("author", 'message', 'private_chat',"time")
+    list_filter = ("author", 'private_chat', "timestamp")
+    search_fields = ("type", "timestamp")
+
+
+    def time(self, model: PrivateChatMessage):
+        date: datetime = model.timestamp.astimezone()
+        format_date = datetime.strftime(date, "%b %d, %H:%M:%S")
+        return format_date
+    
+    def message(self, model: PrivateChatMessage):
+        content = ' '.join(str(model.content).split()[:5])
+        if model.type == "image":
+            return format_html(f'<a href="{content}">تصویر</a>')
+        return content
+    
+
+class GroupChatMessageAdmin(admin.ModelAdmin):
+    model = GroupChatMessage
+    list_display = ("author", 'message', 'room',"time")
+    list_filter = ("author", 'room', "timestamp")
+    search_fields = ("type", "timestamp")
+
+
+    def time(self, model: GroupChatMessage):
+        date: datetime = model.timestamp.astimezone()
+        format_date = datetime.strftime(date, "%b %d, %H:%M:%S")
+        return format_date
+    
+    def message(self, model: GroupChatMessage):
+        content = ' '.join(str(model.content).split()[:5])
+        if model.type == "image":
+            return format_html(f'<a href="{content}">تصویر</a>')
+        return content
+
+
+admin.site.register(GroupChat, GroupChatAdmin)
+admin.site.register(GroupChatMessage, GroupChatMessageAdmin)
+admin.site.register(PrivateChat, PrivateChatAdmin)
+admin.site.register(PrivateChatMessage, PrivateChatMessageAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,111 @@
 from django.db import models
+from django.urls import reverse
+from random import randint, choice
+from django.db.models import QuerySet
+from django.contrib.auth import get_user_model
+from datetime import datetime
+from hashlib import sha256
+from django.utils.crypto import get_random_string
 
 # Create your models here.
+
+
+def unique_group_chat_id() -> str:
+    hash = sha256(str(get_random_string(16)).encode('utf-8')).hexdigest()
+
+    all_chat_id: QuerySet[GroupChat] = GroupChat.objects.all()
+    chat_id = list(map(lambda chat: chat.chat_id, all_chat_id))
+    while hash in chat_id:
+        hash = sha256(str(get_random_string(16)).encode('utf-8')).hexdigest()
+        
+    return hash
+
+
+def unique_private_chat_id() -> str:
+    hash = sha256(str(get_random_string(16)).encode('utf-8')).hexdigest()
+
+    all_chat_id: QuerySet[PrivateChat] = PrivateChat.objects.all()
+    private_chat_id = list(map(lambda chat: chat.chat_id, all_chat_id))
+    while hash in private_chat_id:
+        hash = sha256(str(get_random_string(16)).encode('utf-8')).hexdigest()
+        
+    return str(hash)
+
+
+class GroupChat(models.Model):
+    name = models.CharField(max_length=512, blank=False, null=False)
+    members = models.ManyToManyField(get_user_model(), blank=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    chat_id = models.CharField(
+        max_length=64,
+        default=unique_group_chat_id,
+        blank=False,
+        null=False,
+    )
+    
+
+    class Meta:
+        verbose_name = ("Group Chat")
+        verbose_name_plural = ("Group Chats")
+
+    def __str__(self):
+        return self.name
+    
+
+class PrivateChat(models.Model):
+    author = models.ForeignKey(get_user_model(), blank=False, on_delete=models.CASCADE, related_name='chat_author')
+    contact = models.ForeignKey(get_user_model(), blank=False, on_delete=models.CASCADE, related_name='chat_contanted')
+    timestamp = models.DateTimeField(auto_now_add=True)
+    chat_id = models.CharField(
+        max_length=64,
+        default=unique_private_chat_id,
+        blank=False,
+        null=False,
+    )
+    
+
+    class Meta:
+        verbose_name = ("Private Chat")
+        verbose_name_plural = ("Private Chats")
+
+    def __str__(self):
+        return f"{self.author}/{self.contact}"
+
+
+class AbstractMessage(models.Model):
+    TYPES = (
+        ('message', 'message'),
+        ('image', 'image'),
+    )
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(
+        max_length=32,
+        choices=TYPES,
+        default='message',
+    )
+
+
+    def get_author_username(self):
+        return self.author.username
+
+
+    def get_time(self):
+        time = datetime.strftime(
+            self.timestamp.astimezone(),
+            "%I:%M %p"
+        )
+
+        return time
+
+    class Meta:
+        abstract = True
+
+
+class GroupChatMessage(AbstractMessage):
+    room = models.ForeignKey(GroupChat, on_delete=models.CASCADE, blank=False, null=False)
+
+
+class PrivateChatMessage(AbstractMessage):
+    private_chat = models.ForeignKey(PrivateChat, on_delete=models.CASCADE, blank=False, null=False)
